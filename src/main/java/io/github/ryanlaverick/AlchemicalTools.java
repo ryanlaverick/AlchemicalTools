ae7e821aee76d83663796c050433415a17955705
ryanlaverick <ryan.laverick03@hotmail.co.uk>
2025-04-09 21:00:47 +0100

    Adds temporary listener to ensure event is being fired/consumed as intended

--- src/main/java/io/github/ryanlaverick/AlchemicalTools.java
+++ src/main/java/io/github/ryanlaverick/AlchemicalTools.java
@@ -1,6 +1,7 @@
 package io.github.ryanlaverick;
 
 import io.github.ryanlaverick.listener.CustomToolUsedListener;
+import io.github.ryanlaverick.listener.TriggerCustomToolUsedListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AlchemicalTools extends JavaPlugin {
@@ -9,6 +10,7 @@ public class AlchemicalTools extends JavaPlugin {
     public void onEnable() {
         this.getLogger().info("Plugin Enabled");
 
+        this.getServer().getPluginManager().registerEvents(new TriggerCustomToolUsedListener(), this);
         this.getServer().getPluginManager().registerEvents(new CustomToolUsedListener(), this);
     }
 
--- src/main/java/io/github/ryanlaverick/listener/CustomToolUsedListener.java
+++ src/main/java/io/github/ryanlaverick/listener/CustomToolUsedListener.java
@@ -1,24 +1,18 @@
 package io.github.ryanlaverick.listener;
 
 import io.github.ryanlaverick.event.CustomToolUsedEvent;
-import io.github.ryanlaverick.item.NBTUtility;
-import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.inventory.ItemStack;
 
 public final class CustomToolUsedListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onBreak(BlockBreakEvent event) {
+    public void onUse(CustomToolUsedEvent event) {
         if (event.isCancelled()) return;
 
-        ItemStack itemInHand = event.getPlayer().getInventory().getItemInMainHand();
-
-        if (itemInHand.getType().isAir()) return;
-
-        Bukkit.getServer().getPluginManager().callEvent(new CustomToolUsedEvent(event.getPlayer(), itemInHand));
+        Player player = event.getPlayer();
+        player.sendMessage("Used Tool!");
     }
 }
