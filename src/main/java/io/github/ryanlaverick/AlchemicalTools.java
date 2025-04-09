684dacf1f94bad41171490fcc85f3fece64d926f
ryanlaverick <ryan.laverick03@hotmail.co.uk>
2025-04-09 23:59:39 +0100

    Creates customised configuration files

--- src/main/java/io/github/ryanlaverick/AlchemicalTools.java
+++ src/main/java/io/github/ryanlaverick/AlchemicalTools.java
@@ -1,11 +1,13 @@
 package io.github.ryanlaverick;
 
 import io.github.ryanlaverick.command.AlchemicalToolsCommand;
+import io.github.ryanlaverick.framework.file.ToolFileCache;
 import io.github.ryanlaverick.listener.CustomToolUsedListener;
 import io.github.ryanlaverick.listener.TriggerCustomToolUsedListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AlchemicalTools extends JavaPlugin {
+    private ToolFileCache toolFileCache;
 
     @Override
     public void onEnable() {
@@ -15,10 +17,16 @@ public class AlchemicalTools extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents(new CustomToolUsedListener(), this);
 
         this.getCommand("alchemicaltools").setExecutor(new AlchemicalToolsCommand());
+
+        this.toolFileCache = new ToolFileCache(this);
     }
 
     @Override
     public void onDisable() {
         this.getLogger().info("Plugin Disabled");
+    }
+
+    public ToolFileCache getToolFileCache() {
+        return toolFileCache;
     }
 }
---
+++ src/main/java/io/github/ryanlaverick/framework/file/ToolFileCache.java
@@ -0,0 +1,27 @@
+package io.github.ryanlaverick.framework.file;
+
+import io.github.ryanlaverick.AlchemicalTools;
+import io.github.ryanlaverick.item.Tool;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public final class ToolFileCache {
+    private final Map<Tool, ToolFile> files;
+
+    public ToolFileCache(AlchemicalTools alchemicalTools) {
+        this.files = new EnumMap<>(Tool.class);
+
+        for (Tool tool : Tool.values()) {
+            this.files.put(tool, new ToolFile(tool, alchemicalTools));
+        }
+    }
+
+    public Map<Tool, ToolFile> getFiles() {
+        return files;
+    }
+
+    public ToolFile getFile(Tool tool) {
+        return this.files.get(tool);
+    }
+}
