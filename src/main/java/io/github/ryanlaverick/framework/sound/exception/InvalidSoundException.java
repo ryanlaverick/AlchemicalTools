9f2d0e4f88b58f2ee5f20555f64b64ab95a82326
ryanlaverick <ryan.laverick03@hotmail.co.uk>
2025-04-11 21:57:11 +0100

    Remove unused exception for unparseable Sounds

--- src/main/java/io/github/ryanlaverick/framework/sound/exception/InvalidSoundException.java
+++
@@ -1,7 +0,0 @@
-package io.github.ryanlaverick.framework.sound.exception;
-
-public final class InvalidSoundException extends RuntimeException {
-    public InvalidSoundException(String invalidSound) {
-        super("Unable to parse Sound {sound}".replace("{sound}", invalidSound));
-    }
-}
